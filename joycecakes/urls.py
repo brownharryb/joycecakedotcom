21cd85525013986f02101c2faabe1ce221903e3c
Brown-Harry Boma <brownharryb@gmail.com>
2016-05-09 22:22:34 +0100

    enabled media url

--- joycecakes/urls.py
+++ joycecakes/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     # Examples:
@@ -9,3 +11,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('mainsite.urls'))
 ]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
